a14bc37e0e8fb3585a6b2795adf321cdde480110
Malkollm <fatkullin@bk.ru>
2017-05-10 19:14:18 +0500

    HTTP-запрос и извлечение URL

--- http_server/server.js
+++ http_server/server.js
@@ -1,13 +1,19 @@
 var http = require('http');
+var url = require('url');
 
-function start(){
-http.createServer(function(req, res){
-    console.log("ddsa");
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello world");
-    res.end();
-}).listen(8888);
-console.log("started");
+function start(route){
+    function onRequest(req, res){
+        var pathname = url.parse(req.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        
+        route(pathname);
+        
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Hello world");
+        res.end();
+}
+    http.createServer(onRequest).listen(8888);
+    console.log("started");
 }
 
 exports.start = start;
