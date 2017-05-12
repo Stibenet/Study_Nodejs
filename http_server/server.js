53f93ca7b6415a544f1df6714ed6420e3ec24a12
Malkollm <fatkullin@bk.ru>
2017-05-12 10:21:50 +0500

    Обработка POST-запросов

--- http_server/server.js
+++ http_server/server.js
@@ -3,10 +3,25 @@ var url = require('url');
 
 function start(route, handle){
     function onRequest(req, res){
+        var postData = "";
         var pathname = url.parse(req.url).pathname;
         console.log("Request for " + pathname + " received.");
         
-        route(handle, pathname, res); //callback-function
+        req.setEncoding("utf8");
+        
+        req.addListener("data", function(postDataChunk){
+            //с каждым шагом заполняет переменную при новой порции
+            //POST-данных
+           postData += postDataChunk;
+            console.log("Received POST data chunk '" + 
+                       postDataChunk + "'.");
+        });
+        //указывает что вызов происходит после сбора POST-данных
+        req.addListener("end", function(){
+            //postData передаем для обработчиков запроса
+            route(handle, pathname, res, postData); //callback-function
+        });
+
 }
     http.createServer(onRequest).listen(8888);
     console.log("started");
